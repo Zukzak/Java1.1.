5aed35c0f175b7352c152e7af5132cfd2cd9b58b
Andrey <luftsvine@gmail.com>
2020-12-01 17:49:39 +0300

    Задание 1

--- src/com/andr/practice/Main.java
+++ src/com/andr/practice/Main.java
@@ -2,7 +2,7 @@ package com.andr.practice;
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("Hello World");
 
     }
 }
