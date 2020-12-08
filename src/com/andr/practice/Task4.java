a77c5fac60c7f7ec00a04031d16883b1bb8a2345
Andrey <luftsvine@gmail.com>
2020-12-08 18:49:06 +0300

    Добавлено 4 задание

--- src/com/andr/practice/Task4.java
+++ src/com/andr/practice/Task4.java
@@ -1,7 +1,7 @@
 package com.andr.practice;
 import java.util.Scanner;
 
-public class Main {
+public class Task4 {
     public static void main (String[] args) {
 
         Scanner in = new Scanner(System.in);
