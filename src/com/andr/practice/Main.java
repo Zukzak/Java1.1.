a9b448b9f3e4d050b13ee617b6f9f8452e75cf55
Andrey <luftsvine@gmail.com>
2020-12-08 20:25:41 +0300

    Добавлено 6 задание

--- src/com/andr/practice/Main.java
+++ src/com/andr/practice/Main.java
@@ -3,39 +3,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
-
-        int res;
-
+        double num;
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите первое число ");
+        System.out.println("Выберите что переводить: 1 - масса, 2 - расстояние");
         int x = in.nextInt();
-        System.out.println("Введите второе число ");
-        int y = in.nextInt();
-        System.out.println("Введите арифмитический знак (+, -, /, *)");
-        char z = in.next().charAt(0);
-        in.close();
 
-        switch (z) {
-            case '+' : res = x+y;
-                System.out.println("Сумма числа " + x + " и " + y + " = " + res);
-                break;
-            case '-' : res = x-y;
-                System.out.println("Разница чисел " + x + " и " + y + " = " + res);
-                break;
-            case '*' : res = x*y;
-                System.out.println("Произведение чисел " + x + " и " + y + " = " + res);
-                break;
-            case '/' :
-                if (y==0) {
-                    System.out.println("На ноль делить нельзя!");
-                }
-                else {
-                    res = x/y;
-                    System.out.println("Частное чисел " + x + " и " + y + " = " + res);
-                }
-                break;
+       if (x==2) {
+           System.out.println("Выберите единицу измерения: 1 - метр, 2 - миля, 3 - ярд, 4 - фут");
+           int y = in.nextInt();
+           System.out.println("Введите число");
+           num = in.nextDouble();
+           in.close();
+           switch (y) {
+               case 1: System.out.println("Метры - " + num + " Мили - " + num/1609 + " Ярды - " + num*1.094 + " Футы - " + num*3.281);
+                   break;
+               case 2: System.out.println("Метры - " + num*1609 + " Мили - " + num + " Ярды - " + num*1760 + " Футы - " + num*5280);
+                   break;
+               case 3: System.out.println("Метры - " + num/1.094 + " Мили - " + num/1760 + " Ярды - " + num + " Футы - " + num*3);
+                   break;
+               case 4: System.out.println("Метры - " + num/3.281 + " Мили - " + num/5280 + " Ярды - " + num/3 + " Футы - " + num);
+                   break;
+           }
+       }
+       else if (x==1){
+            System.out.println("Выберите единицу измерения: 1 - грамм, 2 - килограмм, 3-фунт");
+            int z = in.nextInt();
+           System.out.println("Введите число");
+           num = in.nextDouble();
+           switch (z) {
+               case 1: System.out.println("Килограммы - " + num/1000 + " Граммы - " + num + " Фунты - " + num/454);
+                   break;
+               case 2: System.out.println("Килограммы - " + num + " Граммы - " + num*1000 + " Фунты - " + num/2.205);
+                   break;
+               case 3: System.out.println("Килограммы - " + num*2.205 + " Граммы - " + num*454 + " Фунты - " + num);
+                   break;
+           }
         }
-
-
+        }
     }
-}
