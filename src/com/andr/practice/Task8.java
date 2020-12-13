0b51fbce555bae562a8c368cd85d8311356fe683
Andrey <luftsvine@gmail.com>
2020-12-13 20:04:21 +0300

    Добавлено 8 задание

---
+++ src/com/andr/practice/Task8.java
@@ -0,0 +1,19 @@
+package com.andr.practice;
+import java.util.Scanner;
+
+public class Task8 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Введите любое положительное число: ");
+        int n = input.nextInt();
+        while  (n%2==1) {
+            System.out.println("Вы ввели не положительное число, введите положительное число: ");
+            n = input.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            if (i%2==1) sum = sum + i;
+        }
+        System.out.print ("Сумма нечетных чисел до числа - " + n + " равна " + sum);
+    }
+}
