0d8687c1dab75a762918ac3d50d90ef6e969b3ab
Andrey <luftsvine@gmail.com>
2020-12-14 19:59:23 +0300

    Исправлено 8 задание

--- src/com/andr/practice/Task8.java
+++ src/com/andr/practice/Task8.java
@@ -5,12 +5,14 @@ public class Task8 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int sum = 0;
-        System.out.println("Введите любое положительное число: ");
-        int n = input.nextInt();
-        while  (n%2==1) {
-            System.out.println("Вы ввели не положительное число, введите положительное число: ");
+        int n;
+
+        do {
+            System.out.println("Введите любое положительное число: ");
             n = input.nextInt();
         }
+        while  (n%2==1);
+
         for (int i = 0; i < n; i++) {
             if (i%2==1) sum = sum + i;
         }
