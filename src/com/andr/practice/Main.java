0e09755c845dc07f41b215c33284e4a4962dd248
Andrey <luftsvine@gmail.com>
2020-12-24 20:27:43 +0300

    Исправил 14 задание

--- src/com/andr/practice/Main.java
+++ src/com/andr/practice/Main.java
@@ -1,43 +1,31 @@
 package com.andr.practice;
-import java.util.Scanner;
+import java.util.Random;
+
 
 public class Main {
-    public static void main (String[] args) {
-        double num;
-        Scanner in = new Scanner(System.in);
-        System.out.println("Выберите что переводить: 1 - масса, 2 - расстояние");
-        int x = in.nextInt();
+    public static void main(String[] args) {
+        int max = -21;
+        int min = 21;
+        Random random = new Random();
 
-       if (x==2) {
-           System.out.println("Выберите единицу измерения: 1 - метр, 2 - миля, 3 - ярд, 4 - фут");
-           int y = in.nextInt();
-           System.out.println("Введите число");
-           num = in.nextDouble();
-           in.close();
-           switch (y) {
-               case 1: System.out.println("Метры - " + num + " Мили - " + num/1609 + " Ярды - " + num*1.094 + " Футы - " + num*3.281);
-                   break;
-               case 2: System.out.println("Метры - " + num*1609 + " Мили - " + num + " Ярды - " + num*1760 + " Футы - " + num*5280);
-                   break;
-               case 3: System.out.println("Метры - " + num/1.094 + " Мили - " + num/1760 + " Ярды - " + num + " Футы - " + num*3);
-                   break;
-               case 4: System.out.println("Метры - " + num/3.281 + " Мили - " + num/5280 + " Ярды - " + num/3 + " Футы - " + num);
-                   break;
-           }
-       }
-       else if (x==1){
-            System.out.println("Выберите единицу измерения: 1 - грамм, 2 - килограмм, 3-фунт");
-            int z = in.nextInt();
-           System.out.println("Введите число");
-           num = in.nextDouble();
-           switch (z) {
-               case 1: System.out.println("Килограммы - " + num/1000 + " Граммы - " + num + " Фунты - " + num/454);
-                   break;
-               case 2: System.out.println("Килограммы - " + num + " Граммы - " + num*1000 + " Фунты - " + num/2.205);
-                   break;
-               case 3: System.out.println("Килограммы - " + num*2.205 + " Граммы - " + num*454 + " Фунты - " + num);
-                   break;
-           }
+        int[] m = new int[15];
+
+        for (int i=0; i<=m.length-1; i++) {
+            m[i] = random.nextInt(41) - 20;
+            if (max<=m[i]) max = m[i];
+            if (m[i]<=min) min = m[i];
         }
+        System.out.print ("Массив:");
+        for (int j : m) {
+            System.out.print(" " + j);
         }
+        System.out.print ("\n");
+        System.out.println("Максимальный элемент массива: " + max);
+        System.out.println("минимальныйальный элемент массива: " + min);
+
+        int resultMax = Math.max(Math.abs(max),Math.abs(min));
+
+        System.out.println("Наибольший по модулю элемент массива: " + resultMax);
+
     }
+}
--- src/com/andr/practice/Task14.java
+++ src/com/andr/practice/Task14.java
@@ -10,12 +10,15 @@ public class Task14 {
 
         int[] m = new int[15];
 
-        for (int i : m) {
+        for (int i=0; i<=m.length-1; i++) {
             m[i] = random.nextInt(41) - 20;
             if (max<=m[i]) max = m[i];
             if (m[i]<=min) min = m[i];
         }
-
+        System.out.print ("Массив:");
+        for (int j : m) {
+            System.out.print(" " + j);
+        }
         System.out.print ("\n");
         System.out.println("Максимальный элемент массива: " + max);
         System.out.println("минимальныйальный элемент массива: " + min);
