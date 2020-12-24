d22b78d734bf662517bcef3082a09845dea4e0fc
Andrey <luftsvine@gmail.com>
2020-12-24 23:08:35 +0300

    Исправил 4 итоговое задание

--- src/com/andr/practice/FinalTask4.java
+++ src/com/andr/practice/FinalTask4.java
@@ -3,41 +3,87 @@ import java.util.Scanner;
 
 public class FinalTask4 {
     public static void main(String[] args) {
-        int i = 1;
         String a;
-        boolean podsk = false;
         Scanner in = new Scanner(System.in);
 
         System.out.println("Загадка - Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает \n Всего 3 попытки! \n При первой попытке можно написать \"Подсказка\" для получения подсказки.");
 
-        do {
+        System.out.print("Введите отгадку:");
+        a = in.nextLine();
+
+        if (a.equals("Заархивированный вирус")) {
+            System.out.println("Правильно");
+            System.exit(0);
+        }
+        if (a.equals("Подсказка")) {
+            System.out.println("Загадка не про лук, кстати если не отгадаешь сейчас - проиграешь!");
+            System.out.print("Введите отгадку:");
+            a = in.nextLine();
+
+            if ( a.equals("Подсказка")) {
+                System.out.print("Подсказка уже была. Следующая попытка последняя!");
+                System.out.print("Введите отгадку:");
+                a = in.nextLine();
+
+                if (a.equals("Заархивированный вирус")) {
+                    System.out.println("Правильно");
+                    System.exit(0);
+                } else System.out.println("Обидно, приходи в другой раз");
+                System.exit(0);
+            } else System.out.println("Обидно, приходи в другой раз");
+            System.exit(0);
+
+        } else {
+            System.out.println("Подумай еще!");
             System.out.print("Введите отгадку:");
             a = in.nextLine();
             if (a.equals("Заархивированный вирус")) {
                 System.out.println("Правильно");
                 System.exit(0);
             }
+            if (a.equals("Подсказка")) {
+                System.out.print("Подсказка уже недоступна. Следующая попытка будет принята как ответ!");
+                System.out.print("Введите отгадку:");
+                a = in.nextLine();
+                if (a.equals("Заархивированный вирус")) {
+                    System.out.println("Правильно");
+                    System.exit(0);
+                } else {
+                    System.out.println("Подумай еще!");
+                    System.out.print("Введите отгадку:");
+                    a = in.nextLine();
+                    if (a.equals("Подсказка")) {
+                        System.out.print("Подсказка уже недоступна. Следующая попытка будет принята как ответ!");
+                        System.out.print("Введите отгадку:");
+                        a = in.nextLine();
+                        if (a.equals("Заархивированный вирус")) {
+                            System.out.println("Правильно");
+                        } else System.out.println("Обидно, приходи в другой раз");
+                        System.exit(0);
+                    }
 
-            if (i==1&a.equals("Подсказка")) {
-                System.out.println("Загадка не про лук");
-                podsk =true;
-                --i;
-            }
-            if ((i==2|i==3) & a.equals("Подсказка")) {
-                System.out.println("Подсказка уже недоступна");
-                --i;
-            }
-
-            if (i==2&podsk) {
-                System.out.println("Обидно, приходи в другой раз");
+                }
+            } else {
+                System.out.println("Подумай еще!");
+                System.out.print("Введите отгадку:");
+                a = in.nextLine();
+                if (a.equals("Заархивированный вирус")) {
+                    System.out.println("Правильно");
+                    System.exit(0);
+                }
+                if (a.equals("Подсказка")) {
+                    System.out.print("Подсказка уже недоступна. Следующая попытка будет принята как ответ!");
+                    System.out.print("Введите отгадку:");
+                    a = in.nextLine();
+                    if (a.equals("Заархивированный вирус")) {
+                        System.out.println("Правильно");
+                        System.exit(0);
+                    } else System.out.println("Обидно, приходи в другой раз");
+                    System.exit(0);
+                } else System.out.println("Обидно, приходи в другой раз");
                 System.exit(0);
             }
-
-            System.out.println("Подумай еще!");
-            i++;
-
+            System.out.println("Обидно, приходи в другой раз");
         }
-        while (!(i ==4));
-        System.out.println("Обидно, приходи в другой раз");
     }
 }
